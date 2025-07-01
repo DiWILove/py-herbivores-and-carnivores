db9da6ce36680cd02fdd44fefd30c0ba7ae588dd
DiWILove <povilaitusdiana@gmail.com>
2025-07-01 09:09:15 +0300

    flake8_fix

--- app/main.py
+++ app/main.py
@@ -1,42 +1,44 @@
-class Animal:
-    alive = []
+from typing import List
 
-    def __init__(self, name, health=100):
-        self._health = health
-        self.name = name
-        self.hidden = False
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self._health: int = health
+        self.name: str = name
+        self.hidden: bool = False
         if self._health > 0:
             Animal.alive.append(self)
 
     @property
-    def health(self):
+    def health(self) -> int:
         return self._health
 
     @health.setter
-    def health(self, value):
+    def health(self, value: int) -> None:
         self._health = value
         if self._health <= 0 and self in Animal.alive:
             Animal.alive.remove(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             f"{{Name: {self.name}, Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, animal):
+    def bite(self, animal: Animal) -> None:
         if not isinstance(animal, Herbivore):
             return
-
         if not animal.hidden:
             animal.health -= 50
